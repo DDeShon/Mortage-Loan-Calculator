d0a88263a172157525db54882a0591f933eebf3f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-04-01 03:24:42 -0500

    adjusted the sizing of the content to prevent overflow

--- Script/mortgage-calculator.js
+++ Script/mortgage-calculator.js
@@ -1,31 +1,54 @@
+document.getElementById("calc-button").addEventListener("click", calculate);
+
+function calculate() {
+  // calcLength();
+  calcPrice();
+  // calcDown();
+}
+
+// calculate net price
+function calcPrice() {
+  var price = document.getElementById("home-price").value;
+  var netPrice = price - calcDown();
+  console.log(netPrice);
+  return netPrice;
+}
+
+// calculate down payment/percent:  price * down_percent OR price / down_payment
+function calcDown() {
+  var downPayment = document.getElementById("down-payment").value;
+  var downPercent = document.getElementById("down-percent").value;
+  // console.log(downPayment, downPercent);
+  var down = null;
+  if (downPayment == "") {
+    down = downPercent * 0.01;
+    // console.log(down);
+    // return down;
+  } else if (downPercent == "") {
+    down = downPayment;
+    // console.log(down);
+    // return down;
+  } else {
+    down = null;
+  }
+  // console.log(down);
+  return down;
+}
+
 // calculate loan length in months
 function calcLength() {
   var loanLength = document.getElementById("loan-length").value;
-  console.log(loanLength);
-  var loanMonths = loanLength / 12;
-  return loanMonths;
+  var loanMonths = loanLength * 12;
+  console.log(loanMonths);
+  // return loanMonths;
 }
 
 // calculate interest as a decimal:  (price - down) * interest rate
 function calcInterest() {
   var interestRate = document.getElementById("interest-rate").value * 0.01;
   var interestMonthly = price * interestRate;
+  console.log(interestMonthly);
   return interestMonthly;
-}
-
-// calculate down payment/percent:  price * down_percent OR price / down_payment
-function calcDown() {
-  // var downPayment = document.getElementById("down-payment").value;
-  // var downPercent = document.getElementById("down-percent").value;
-  var down = null;
-  return down;
-}
-
-// calculate net price
-function calcPrice() {
-  var price = document.getElementById("home-price").value;
-  var netPrice = price - down;
-  return netPrice;
 }
 
 // calculate property tax
